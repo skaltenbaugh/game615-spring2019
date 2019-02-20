e53d6dfb04b29bdba8a49d1e8133f5b94be7a79c
skaltenbaugh <46975784+skaltenbaugh@users.noreply.github.com>
2019-02-20 14:45:31 -0500

    added exercise03 and exercise04 (blank)

---
+++ project03/Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+                
+    }
+}
