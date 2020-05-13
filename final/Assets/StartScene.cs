b5f20734fa1889dafc196bb222613ca3190ab54f
skaltenbaugh <46975784+skaltenbaugh@users.noreply.github.com>
2020-05-13 17:46:39 -0400

    added final

---
+++ final/Assets/StartScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+ 
+    public void StartGame(int GameStart)
+    {
+        SceneManager.LoadScene(GameStart)
+    }
+        
+    }
